3dc79e2a346b03d06a8eaae1f831cb4c95c16a29
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-02-09 14:04:27 +0100

    add spotlightpage component and move fetch function to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,31 @@
 import GlobalStyle from "../styles";
 import Header from "@/components/Header";
 import "../components/Header.css";
+import useSWR from "swr";
 
+
+const fetcher = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
 export default function App({ Component, pageProps }) {
+    const { data, error, isLoading, isValidating } = useSWR(
+    "https://example-apis.vercel.app/api/art",
+    fetcher
+  );
   return (
     <>
       <GlobalStyle />
       <Header/>
       <main className="app-main">
-        <Component {...pageProps} />
+        <Component {...pageProps} 
+        data={data}
+        error={error}
+        isLoading={isLoading}
+        isValidating={isValidating}
+        
+        />
       </main>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,17 @@ import Card from "@/components/Card";
 import useSWR from "swr";
 import Image from "next/image";
 
-const fetcher = async (url) => {
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
-};
+// const fetcher = async (url) => {
+//   const response = await fetch(url);
+//   const data = await response.json();
+//   return data;
+// };
 
-export default function HomePage() {
-  const { data, error, isLoading, isValidating } = useSWR(
-    "https://example-apis.vercel.app/api/art",
-    fetcher
-  );
+export default function HomePage({data, error, isLoading, isValidating}) {
+  // const { data, error, isLoading, isValidating } = useSWR(
+  //   "https://example-apis.vercel.app/api/art",
+  //   fetcher
+  // );
 
   if (error) return <div>failed to load</div>;
   if (isLoading) return <div>loading...</div>;
@@ -20,6 +20,7 @@ export default function HomePage() {
 
   return (
     <>
+
       {data.map((item) => {
         return (
           <Card key={item.slug} title={item.artist} description={item.name}>
