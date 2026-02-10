38ec434138ed9f118e4146400958400bb444be1c
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-02-10 21:09:09 +0100

    add style to form

--- components/CommentInput.js
+++ components/CommentInput.js
@@ -1,12 +1,38 @@
+import styled from "styled-components";
+
 export default function CommentInput({ onSubmit }) {
   return (
-    <div>
+    <FormContainer>
       <form onSubmit={onSubmit}>
-        <label htmlFor="comment">Comment: </label>
-        <input type="text" name="comment" id="comment" />
-        <br />
-        <button type="submit">Send</button>
+        {/* <label htmlFor="comment">Comment: </label> */}
+        <Input type="text" name="comment" id="comment" placeholder="Comment" />
+        <Button type="submit">Send</Button>
       </form>
-    </div>
+    </FormContainer>
   );
 }
+
+const FormContainer = styled.div`
+height: 100px;
+width: 250px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+ali
+gap: 20px;
+`;
+const Input = styled.input`
+  width: 230px;
+  min-height: 40px;
+  border-radius: 60px;
+  padding: 10px;
+`;
+const Button = styled.button`
+  padding: 10px 20px;
+  background-color: blue;
+  color: white;
+  border-radius: 12px;
+  border: none;
+  cursor: pointer;
+`;
